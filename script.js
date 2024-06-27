fd4d53e2849f76d3161d004bc837e975ad27203e
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2024-06-27 17:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEvenetListener("submit", (e) =>{
+
+	e.preventDefault();
+	let fontsize = document.getElementByid("fontsize").value;
+	let fontcolor = document.getElementByid("fontcolor").value;
+
+	document.cookie = `fontsize=$(fontsize);`
+	document.cookie = `fontcolor=$(fontcolor);`
+})
+
+let cookies = Object.fromEntries(
+	document.cookie.split("; "),
+	map((item) => item.split("#")));
+
+document.getElementByid("fontsize").value = cookies.fontsize??0;
+document.getElementByid("fontcolor").value = cookies.fontcolor??"#000"
