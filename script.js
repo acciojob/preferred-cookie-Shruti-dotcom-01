77b1bb5acdf6de19d2708f0056af295c588ebd67
Shruti-Vaidya <147685533+Shruti-dotcom-01@users.noreply.github.com>
2024-06-27 17:31:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.getElementById("myForm").addEventListener("submit", (e) =>{
 	let fontsize = document.getElementById("fontsize").value;
 	let fontcolor = document.getElementById("fontcolor").value;
 
-	document.cookie = `fontsize=$(fontsize);`
-	document.cookie = `fontcolor=$(fontcolor);`
+	document.cookie = `fontsize=${fontsize};`
+	document.cookie = `fontcolor=${fontcolor};`
 })
 
 let cookies = Object.fromEntries(
 	document.cookie.split("; ").
 	map((item) => item.split("=")));
 
-document.getElementById("fontsize").value = cookies.fontsize??0;
-document.getElementById("fontcolor").value = cookies.fontcolor??"#000"
+document.getElementById("fontsize").value = cookies.fontsize ?? 0;
+document.getElementById("fontcolor").value = cookies.fontcolor ?? "#000";
